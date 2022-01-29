cd1bf1d97801657639e6a3a760f9eeeddd83b0ca
Maciek <maciek.backwater@gmail.com>
2022-01-30 00:06:40 +0100

    Resource gathering - assignment

---
+++ Assets/Tests/AI/ResourceGatheringAITests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polyjam_2022.Tests
+{
+    public class ResourceGatheringAITests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            ResourceGatheringAI gatherer = new ResourceGatheringAI();
+        }
+    }
+}
