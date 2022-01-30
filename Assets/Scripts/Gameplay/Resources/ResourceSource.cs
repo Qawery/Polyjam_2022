716441b4035865c1b1419650b400380df5d36d3e
Maciek <maciek.backwater@gmail.com>
2022-01-30 02:16:11 +0100

    ResourceSource has now BlockedAmountPerType (when units are on their way)

--- Assets/Scripts/Gameplay/Resources/ResourceSource.cs
+++ Assets/Scripts/Gameplay/Resources/ResourceSource.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace Polyjam_2022
 {
@@ -10,9 +12,14 @@ namespace Polyjam_2022
 
         public ResourceManager Resources { get; private set; }
 
+        public Dictionary<ResourceType, int> BlockedAmountPerType { get; set; } = new Dictionary<ResourceType, int>();
         private void Awake()
         {
             Resources = new ResourceManager(maxCapacity, startingAmmount);
+            foreach (var resourceType in Enum.GetValues(typeof(ResourceType)).Cast<ResourceType>())
+            {
+                BlockedAmountPerType.Add(resourceType, 0);
+            }
         }
     }
 }
