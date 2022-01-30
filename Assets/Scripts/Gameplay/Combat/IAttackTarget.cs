09da3e12352286e41fc079271d66803c3094b549
Wooki <wooki1@op.pl>
2022-01-30 14:02:26 +0100

    AttackEnemies task.

---
+++ Assets/Scripts/Gameplay/Combat/IAttackTarget.cs
@@ -0,0 +1,6 @@
+namespace Polyjam_2022
+{
+    public interface IAttackTarget : IPositionProvider, IDestructible
+    {
+    }
+}
---
+++ Assets/Tests/AI/Effects/MoveTowardsTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Polyjam_2022.Tests
+{
+    public class MoveTowardsTests
+    {
+        [Test]
+        public void MoveTowardsTest()
+        {
+        }
+    }
+}
