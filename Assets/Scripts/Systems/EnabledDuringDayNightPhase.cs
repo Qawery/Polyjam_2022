995ecad6d4b54520f6f8a7fce1a4dbd00969d34c
rtyl <rafal.tyl@qed.pl>
2022-01-30 05:43:58 +0100

    Switch the building panel off at night

---
+++ Assets/Scripts/Systems/EnabledDuringDayNightPhase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Polyjam_2022
+{
+    public class EnabledDuringDayNightPhase : MonoBehaviour
+    {
+        [SerializeField] private bool enabledDuringDayPhase = true;
+
+        [Inject]
+        private void Init(IDayNightCycle dayNightCycle)
+        {
+            dayNightCycle.OnCycleChanged += cycle =>
+            {
+                gameObject.SetActive(cycle.IsDay == enabledDuringDayPhase);
+
+            };
+        }
+    }
+}
