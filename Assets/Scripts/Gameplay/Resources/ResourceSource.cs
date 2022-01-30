094c203c73124ee0d00af2e0b34a42eb788b0707
rtyl <rafal.tyl@qed.pl>
2022-01-30 11:07:53 +0100

    Create resource source prefabs

--- Assets/Scripts/Gameplay/Resources/ResourceSource.cs
+++ Assets/Scripts/Gameplay/Resources/ResourceSource.cs
@@ -8,14 +8,14 @@ namespace Polyjam_2022
     public class ResourceSource : MonoBehaviour, IResourceHolder
     {
         [SerializeField, Range(0, 1000)] private int maxCapacity = 10;
-        [SerializeField, Range(0, 1000)] private List<(ResourceType type, int amount)> startingAmmount;
+        [SerializeField] private List<ResourceRequirement> startingAmount;
 
         public ResourceManager Resources { get; private set; }
 
         public Dictionary<ResourceType, int> BlockedAmountPerType { get; set; } = new Dictionary<ResourceType, int>();
         private void Awake()
         {
-            Resources = new ResourceManager(maxCapacity, startingAmmount);
+            Resources = new ResourceManager(maxCapacity, startingAmount.Select(data => (data.ResourceType, data.RequiredAmount)));
             foreach (var resourceType in Enum.GetValues(typeof(ResourceType)).Cast<ResourceType>())
             {
                 BlockedAmountPerType.Add(resourceType, 0);
